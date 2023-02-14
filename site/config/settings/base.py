91976896b266e201837a727a681df4b67b2a4d81
Gadzet <saygitovg@gmail.com>
2023-02-14 19:36:24 +0300

    Небольшие изменения settings

--- site/config/settings/base.py
+++ site/config/settings/base.py
@@ -31,7 +31,7 @@ LOCAL_APPS = [
     'common.apps.CommonConfig',
     'users.apps.UsersConfig',
     'catalog.apps.CatalogConfig',
-    'queries.apps.QueriesConfig'
+    'queries.apps.QueriesConfig',
     'rating.apps.RatingConfig',
 ]
 INSTALLED_APPS = DJANGO_APPS + MODULE_APPS + LOCAL_APPS
