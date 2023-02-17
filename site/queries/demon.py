1a0bfec7dac42f210a8a54327026362f19d8d64f
Unknown <user@localhost.localdomain>
2023-02-17 11:55:28 +0300

    небольшие изменения

--- site/queries/demon.py
+++ site/queries/demon.py
@@ -1,20 +1,22 @@
 from queries.request import SiteQueryManager
-from time import sleep
 from datetime import datetime
 from catalog.models import Site
 
 
 def demon():
     start_time = datetime.now()
-    print("START PROCESS")
+    print('START PROCESS')
 
     sites = Site.objects.all()
-    manager = SiteQueryManager(sites)
-    sites = manager.get_sites_info()
-    for site in sites:
-        print(site.name, site.url, site.status_code, site.ping, sep='\t')
-    manager.save()
+    try:
+        manager = SiteQueryManager(sites)
+        sites = manager.get_sites_info()
+        for site in sites:
+            print(site.name, site.url, site.status_code, site.ping, sep='\t')
+        manager.save()
+    except Exception:
+        print('ERROR')
 
     finish_time = datetime.now()
-    print("FINISH PROCESS")
+    print('FINISH PROCESS')
     print('DURATION:', finish_time - start_time)
