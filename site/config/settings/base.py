154eeba586d658c1bcb571e46b99a565d80156a8
Gadzet <saygitovg@gmail.com>
2023-02-18 15:15:22 +0300

    уллучшил графики

--- site/config/settings/base.py
+++ site/config/settings/base.py
@@ -96,10 +96,10 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 LANGUAGE_CODE = 'ru'
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 USE_I18N = True
 USE_L10N = True
-USE_TZ = True
+USE_TZ = False
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
--- site/core/templatetags/common.py
+++ site/core/templatetags/common.py
@@ -1,5 +1,8 @@
+import json
+
 from django import template
-from django.utils import timezone
+from django.core.serializers.json import DjangoJSONEncoder
+from django.db.models import F
 
 
 register = template.Library()
@@ -10,10 +13,26 @@ def get_site_status(site):
     last_query_note = site.query_notes.first()
     if not last_query_note:
         return None
-    
+
     ping = last_query_note.ping
     status_code = last_query_note.status_code
 
     return {
         'ping': ping, 'status_code': status_code
         }
+
+
+@register.simple_tag()
+def site_query_notes_to_json(site):
+    query_notes = list(
+        site.query_notes
+        .filter(ping__isnull=False)
+        .reverse()
+        .annotate(
+            hour=F('note_time__hour'),
+            minute=F('note_time__minute'),
+        )
+        .values('ping', 'hour', 'minute')
+        )
+
+    return json.dumps(query_notes, cls=DjangoJSONEncoder)
