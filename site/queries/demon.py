f1a6d40210b38ed94ea9f2db26a60ce9ea8b6072
Gadzet005 <88264036+Gadzet005@users.noreply.github.com>
2023-02-19 15:49:30 +0300

    Изменил README + разделение на requirements prod и dev (#12)
    * изменил ридми, добавил requirements-prod.txt
    
    * Update README.md
    
    * Изменил .gitignore + env.example
    
    * фикс
    
    * Update README.md
    
    * правки

--- site/queries/demon.py
+++ site/queries/demon.py
@@ -6,6 +6,4 @@ def demon():
     sites = Site.objects.all()
     manager = SiteQueryManager(sites)
     sites = manager.get_sites_info()
-    for site in sites:
-        print(site.name, site.url, site.status_code, site.ping)
     manager.save()
