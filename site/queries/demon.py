330e25fc169377add508799482f3eda8c4d4ce3f
kalibdune <kalibdune@yadex.ru>
2023-02-20 20:53:25 +0300

    демон капелька изменений

--- site/queries/demon.py
+++ site/queries/demon.py
@@ -1,29 +1,34 @@
 import re
 from queries.request import SiteQueryManager
-from catalog.models import Site
 from queries.models import SiteFallReason
+from catalog.models import Site
 from datetime import datetime
+from common.utils import notify_users
+
 
 def down(site):
-    sites=SiteFallReason.objects.all()
+    sites = SiteFallReason.objects.all()
     if len(sites) == 0:
-        st="this site fall "+ site.url
-        SiteFallReason.objects.create(site=site, reason="ping > 5 seconds")
-        return st
+        reason = f'Наш сервер обнаружил, что сайт {site.url}'
+        SiteFallReason.objects.create(site=site, reason=reason)
+        notify_users(site.url, site.status_code, reason)
+        return 'fall'
     for FallSite in sites:
         if site.url == FallSite.site.url:
             return None
-        st="this site fall "+ site.url
-        SiteFallReason.objects.create(site=site, reason="ping > 5 seconds")
-        return st
+        reason = f'Наш сервер обнаружил, что сайт {site.url}'
+        SiteFallReason.objects.create(site=site, reason=reason)
+        notify_users(site.url, site.status_code, reason)
+        return 'fall'
+
+
 def up(site):
-    sites=SiteFallReason.objects.all()
+    sites = SiteFallReason.objects.all()
     for FallSite in sites:
         if site.url == FallSite.site.url:
             FallSite.delete()
-            print("delete")
-                
-
+            reason = f'Наш сервер обнаружил, что сайт {site.site.url}'
+            notify_users(site.site.url, site.status_code, reason)
 
 
 def demon():
@@ -36,8 +41,9 @@ def demon():
     for site in sites:
         print(site.name, site.url, site.status_code, site.ping, sep='\t')
     manager.save()
+    print(SiteFallReason.objects.all())
     for site in sites:
-        if site.ping==2000:
+        if site.ping == 2000:
             print(down(site))
         else:
             up(site)
@@ -45,4 +51,3 @@ def demon():
     finish_time = datetime.now()
     print("FINISH PROCESS")
     print('DURATION:', finish_time - start_time)
-
